b1227a6626238b119c62c06e4ff0822e1440ef67
Nermin Skenderovic <nermin@skenderovic.se>
2026-05-12 18:38:15 +0200

    Improve SEO

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/skanetrafiken-delays/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
